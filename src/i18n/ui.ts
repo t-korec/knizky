971f4e534775edafdf4fb610f3f29157d368594c
tt-korec <t.korec@tricentis.com>
2024-04-30 09:19:42 +0200

    feat: barely firt page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  en: 'English',
+  cz: 'Česky',
+};
+
+export const defaultLang = 'cz';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.aInstein': 'AInstein',
+    'nav.languageProofreading': 'Language Proofreading',
+    'nav.plagiarismCheck': 'Plagiarism check',
+    'nav.review': 'Review',
+    'nav.blog': 'Blog',
+    'nav.contact.chat': 'Chat',
+    'nav.contact.online': "we're online",
+    'nav.contact.weekdays': 'Mon-Fri: 8:00-17:00',
+  },
+  cz: {
+    'nav.home': 'Vazba a Tisk',
+    'nav.aInstein': 'AInstein',
+    'nav.languageProofreading': 'Jazykové korektury',
+    'nav.plagiarismCheck': 'Kontrola plagiátů',
+    'nav.review': 'Recenze',
+    'nav.premium': 'SuperVazby Premium',
+    'nav.blog': 'Blog',
+    'nav.contact.chat': 'Napiště nám',
+    'nav.contact.online': 'jsme online',
+    'nav.contact.weekdays': 'Po-Pá: 8:00-17:00',
+  },
+} as const;
